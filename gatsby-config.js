fd79fdd025fd4a9451330846e5645e3c387588ab
Patrick Mason <patrickm.mason@gmail.com>
2021-03-01 18:16:09 +1100

    add gatsby-image-remark and gatsby-remark-relative-images capabilities and a feature image widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,11 +11,17 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/static/assets`,
+        name: "netlifycms_images",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `markdown-pages`,
         path: `${__dirname}/src/blog`,
       },
     },
-    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -35,6 +41,23 @@ module.exports = {
         theme_color: `#fff`,
         display: `minimal-ui`,
         icon: `src/images/tabula_rasa_favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/blog/this-is-a-test-post.md
+++ src/blog/this-is-a-test-post.md
@@ -2,10 +2,8 @@
 path: /blog/test-post
 date: 2021-02-21T10:20:11.054Z
 title: This is a test post
-image: /src/blog/assets/ig_orig.jpg
 ---
-# *This is the body of a test post*
 
-
+# _This is the body of a test post_
 
 ![](/assets/ig_orig.jpg)
